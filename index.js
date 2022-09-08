da174d7abd451f2369411f375dafbc2e081bdcd7
Berk Altıok <berkpw@gmail.com>
2022-09-08 12:14:11 +0300

    feat: create variant plugin

---
+++ index.js
@@ -0,0 +1,19 @@
+const postcss = require('postcss')
+
+module.exports = function() {
+	return function ({ addVariant, e, theme }) {
+		const screens = theme('screens')
+		Object.keys(screens).map((screen) => {
+			addVariant(`<${screen}`, ({ container, separator }) => {
+				const mediaRule = postcss.atRule({ name: 'media', params: `(max-width: ${screens[screen]})` })
+				mediaRule.append(container.nodes)
+				container.append(mediaRule)
+				mediaRule.walkRules((rule) => {
+					rule.selector = `.${e(
+						`<${screen}${separator}${rule.raws?.tailwind.classCandidate ?? rule.selector.slice(1)}`
+					)}`
+				})
+			})
+		})
+	}
+}
