c3f19791461b41420dfb560cd5c4210e8bfbe22a
Berk Altıok <berkpw@gmail.com>
2022-09-08 16:33:28 +0300

    fix: screens serialize

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const postcss = require('postcss')
 
-module.exports = function() {
-	return function ({ addVariant, e, theme }) {
-		const screens = theme('screens')
-		Object.keys(screens).map((screen) => {
-			addVariant(`<${screen}`, ({ container, separator }) => {
-				const mediaRule = postcss.atRule({ name: 'media', params: `(max-width: ${screens[screen]})` })
-				mediaRule.append(container.nodes)
-				container.append(mediaRule)
-				mediaRule.walkRules((rule) => {
-					rule.selector = `.${e(
-						`<${screen}${separator}${rule.raws?.tailwind.classCandidate ?? rule.selector.slice(1)}`
-					)}`
-				})
-			})
-		})
-	}
+module.exports = function () {
+  return function ({ addVariant, e, theme }) {
+    const screens = theme('screens')
+    Object.keys(screens)
+      .reverse()
+      .map((screen) => {
+        addVariant(`<${screen}`, ({ container, separator }) => {
+          const mediaRule = postcss.atRule({ name: 'media', params: `(max-width: ${screens[screen]})` })
+          mediaRule.append(container.nodes)
+          container.append(mediaRule)
+          mediaRule.walkRules((rule) => {
+            rule.selector = `.${e(
+              `<${screen}${separator}${rule.raws?.tailwind.classCandidate ?? rule.selector.slice(1)}`
+            )}`
+          })
+        })
+      })
+  }
 }
